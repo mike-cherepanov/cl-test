cb541134c9715e363eb1c8a5cb68c24a2c6988f9
mike-cherepanov <michael.cherepanov.sv@gmail.com>
2024-11-12 18:33:53 +0700

    added Question list view for all and for allowed only questions

--- app/questions/urls.py
+++ app/questions/urls.py
@@ -1,8 +1,9 @@
+from django.shortcuts import redirect
 from django.urls import include, path
 from rest_framework.routers import DefaultRouter
 from rest_framework_nested.routers import NestedDefaultRouter
 
-from .views import ChoicesViewSet, QuestionViewSet
+from .views import ChoicesViewSet, QuestionsListView, QuestionViewSet
 
 
 router = DefaultRouter()
@@ -10,8 +11,14 @@ router.register(r'questions', QuestionViewSet, basename='questions')
 choices_router = NestedDefaultRouter(router, r'questions', lookup='question')
 choices_router.register(r'choices', ChoicesViewSet, basename='question-choices')
 
-urlpatterns = [
+api_url_patters = [
     path('', include(router.urls)),
     path('', include(choices_router.urls)),
+]
+
+urlpatterns = [
+    path('api/', include(api_url_patters)),
+    path('', lambda *args, **kwargs: redirect('all-question-list', 'all')),
+    path('questions/<str:page>', QuestionsListView.as_view(), name='all-question-list'),
     path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
 ]
